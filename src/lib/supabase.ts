5db1abc00d829935cdb946ba90ebebe2f9a0dbd6
Wildan <wabimantoro@gmail.com>
2024-09-29 11:16:35 +0700

    change public credential to private

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY as string;
+const supabaseUrl = process.env.SUPABASE_URL as string;
+const supabaseKey = process.env.SUPABASE_KEY as string;
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error('Missing Supabase URL or Key. Please add them to your environment variables.');
