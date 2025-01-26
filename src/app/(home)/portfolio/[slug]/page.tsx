48957499a8257589314eb8aaae011b5230313d17
wildan <wabimantoro@gmail.com>
2025-01-26 12:21:40 +0700

    adjust mobile size

--- src/app/(home)/portfolio/[slug]/page.tsx
+++ src/app/(home)/portfolio/[slug]/page.tsx
@@ -11,10 +11,10 @@ const PortfolioDetail = ({ params }: { params: { slug: string } }) => {
   return (
     <div className="max-w-[1200px] flex flex-col gap-4 justify-center items-center my-5">
       <div className="text-xl my-10 lg:text-3xl font-medium">{data.title}</div>
-      <div className="w-[600px] h-[300px] flex justify-center">
+      <div className=" w-[350px] h-[300px] lg:w-[600px] lg:h-[300px] flex justify-center">
         <ImgSlider imgListUrl={data.images} delay={3000} />
       </div>
-      <div className="px-10 m-10 text-grey-description text-sm lg:text-base">
+      <div className="px-2 m-2 lg:px-10 lg:m-10 text-grey-description text-sm lg:text-base">
         {data.description}
       </div>
       <div>
